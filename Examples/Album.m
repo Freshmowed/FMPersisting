c402b4a0f5c99d5d6dc54e668485db49a4af9f2f
Duke Browning <duke@freshmowed.com>
2015-06-25 15:09:49 -0500

    Added to Examples

---
+++ Examples/Album.m
@@ -0,0 +1,21 @@
+#import "Album.h"
+
+@implementation Album 
+
++ (NSString *) tableName;
+{
+    return @"ALBUM";
+}
+
++ (NSDictionary *) columns;
+{
+    return @{@"ID" : @"int",
+             @"MEDIA_KEY" : @"string",
+             @"LAST_PLAYED_DATE" : @"date",
+             @"COVER_IMAGE_DATA" : @"data",
+             @"TITLE" : @"string",
+             @"ARTIST" : @"string",
+             @"DESCRIP" : @"string",
+             @"PUBLISH_DATE" : @"date",
+             @"FLAGS" : @"int" };
+}
